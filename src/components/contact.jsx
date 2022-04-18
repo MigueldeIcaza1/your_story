c4fdcdd48f200b563679abb42124f8ea6c97b25a
mgunti <mahalakshmi.gunti@realpage.com>
2022-04-18 14:09:14 +0530

    small UI fixes

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -10,9 +10,9 @@ export class Contact extends React.Component {
 
                         <div className='col-md-2'>
                             <nav aria-label="breadcrumb">
-                                <ol class="breadcrumb">
-                                    <li class="breadcrumb-item"><a href="home">Home</a></li>
-                                    <li class="breadcrumb-item active" aria-current="page">Contact</li>
+                                <ol className="breadcrumb">
+                                    <li className="breadcrumb-item"><a href="home">Home</a></li>
+                                    <li className="breadcrumb-item active" aria-current="page">Contact</li>
                                 </ol>
                             </nav>
                         </div>
@@ -20,15 +20,15 @@ export class Contact extends React.Component {
                         <div className='col-md-10 d-flex'>
                             <div className='col-md-5 leadspace leadspace-padding'>
                                 <div className='col-md-8'>
-                                    <h1 class="leadSpace_header">Contact WSEG Research</h1>
+                                    <h1 className="leadSpace_header">Contact WSEG Research</h1>
                                     <p>Please fill out the form below so the appropriate representative from WESG Research can contact you.</p>
                                 </div>
                             </div>
 
-                            <div class="col-md-6">
+                            <div className="col-md-6 d-none d-sm-block">
                                 <div role="presentation" className="col-md-7 float-end line_space Lines_container__36j4i">
-                                    <svg className="Lines_image__2O61U Lines_imageTop__27foq" height="0" viewBox="0 0 100 100" width="0" xmlns="http://www.w3.org/2000/svg"><line class="Lines_line__2-BFp" x1="-25%" y1="50%" x2="125%" y2="50%" vector-effect="non-scaling-stroke"></line></svg>
-                                    <svg className="Lines_image__2O61U Lines_imageBottom__12Ll5" height="0" viewBox="0 0 100 100" width="0" xmlns="http://www.w3.org/2000/svg"><line class="Lines_line__2-BFp" x1="-25%" y1="50%" x2="125%" y2="50%" vector-effect="non-scaling-stroke"></line></svg>
+                                    <svg className="Lines_image__2O61U Lines_imageTop__27foq" height="0" viewBox="0 0 100 100" width="0" xmlns="http://www.w3.org/2000/svg"><line className="Lines_line__2-BFp" x1="-25%" y1="50%" x2="125%" y2="50%" vectorEffect="non-scaling-stroke"></line></svg>
+                                    <svg className="Lines_image__2O61U Lines_imageBottom__12Ll5" height="0" viewBox="0 0 100 100" width="0" xmlns="http://www.w3.org/2000/svg"><line className="Lines_line__2-BFp" x1="-25%" y1="50%" x2="125%" y2="50%" vectorEffect="non-scaling-stroke"></line></svg>
                                 </div>
                             </div>
 
@@ -42,89 +42,89 @@ export class Contact extends React.Component {
                     <form className='row m-0 p-0'>
                         <div className='col-md-2'></div>
                         <div className='col-md-9 pb-5'>
-                            <div class="form_note float-start">All fields are required unless marked optional.</div>
+                            <div className="form_note float-start">All fields are required unless marked optional.</div>
                             <br/>
 
                             <div className='pt-3'>
-                                <div class="row row-cols-2">
+                                <div className="row">
 
-                                    <div class="col-md-5 pb-4">
+                                    <div className="col-md-5 pb-4">
                                         <div className="bx--form-item bx--text-input-wrapper">
-                                            <label for="firstName" className="bx--label">First name</label>
-                                            <div class="bx--text-input__field-outer-wrapper">
+                                            <label htmlFor="firstName" className="bx--label">First name</label>
+                                            <div className="bx--text-input__field-outer-wrapper">
                                                 <div className="bx--text-input__field-wrapper">
-                                                    <input type="text" placeholder="Ada" class="bx--text-input bx--text__input" title="" name="firstName" />
+                                                    <input type="text" placeholder="Ada" className="bx--text-input bx--text__input" title="" name="firstName" />
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
 
-                                    <div class="col-md-5 pb-4">
+                                    <div className="col-md-5 pb-4">
                                         <div className="bx--form-item bx--text-input-wrapper">
-                                            <label for="lastName" className="bx--label">Last name</label>
-                                            <div class="bx--text-input__field-outer-wrapper">
+                                            <label htmlFor="lastName" className="bx--label">Last name</label>
+                                            <div className="bx--text-input__field-outer-wrapper">
                                                 <div className="bx--text-input__field-wrapper">
-                                                    <input type="text" placeholder="Lovelace" class="bx--text-input bx--text__input" title="" name="lastName" />
+                                                    <input type="text" placeholder="Lovelace" className="bx--text-input bx--text__input" title="" name="lastName" />
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
 
-                                    <div class="col-md-5 pb-4">
+                                    <div className="col-md-5 pb-4">
                                         <div className="bx--form-item bx--text-input-wrapper">
-                                            <label for="emailAddress" className="bx--label">Email address</label>
-                                            <div class="bx--text-input__field-outer-wrapper">
+                                            <label htmlFor="emailAddress" className="bx--label">Email address</label>
+                                            <div className="bx--text-input__field-outer-wrapper">
                                                 <div className="bx--text-input__field-wrapper">
-                                                    <input type="text" placeholder="Lovelace@example.com" class="bx--text-input bx--text__input" title="" name="emailAddress" />
+                                                    <input type="text" placeholder="Lovelace@example.com" className="bx--text-input bx--text__input" title="" name="emailAddress" />
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
 
-                                    <div class="col-md-5 pb-4">
+                                    <div className="col-md-5 pb-4">
                                         <div className="bx--form-item bx--text-input-wrapper">
-                                            <label for="confirmEmailAddress" className="bx--label">Confirm email address</label>
-                                            <div class="bx--text-input__field-outer-wrapper">
+                                            <label htmlFor="confirmEmailAddress" className="bx--label">Confirm email address</label>
+                                            <div className="bx--text-input__field-outer-wrapper">
                                                 <div className="bx--text-input__field-wrapper">
-                                                    <input type="text" placeholder="Lovelace@example.com" class="bx--text-input bx--text__input" title="" name="confirmEmailAddress" />
+                                                    <input type="text" placeholder="Lovelace@example.com" className="bx--text-input bx--text__input" title="" name="confirmEmailAddress" />
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
 
-                                    <div class="col-md-5 pb-4">
+                                    <div className="col-md-5 pb-4">
                                         <div className="bx--form-item bx--text-input-wrapper">
-                                            <label for="phone" className="bx--label">Phone number (optional)</label>
-                                            <div class="bx--text-input__field-outer-wrapper">
+                                            <label htmlFor="phone" className="bx--label">Phone number (optional)</label>
+                                            <div className="bx--text-input__field-outer-wrapper">
                                                 <div className="bx--text-input__field-wrapper">
-                                                    <input type="tel" placeholder="000-123-4567" class="bx--text-input bx--text__input" title="" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"/>
+                                                    <input type="tel" placeholder="000-123-4567" className="bx--text-input bx--text__input" title="" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"/>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
 
-                                    <div class="col-md-5 pb-4">
+                                    <div className="col-md-5 pb-4">
                                         <div className="bx--form-item bx--text-input-wrapper">
-                                            <label for="phone" className="bx--label">Job role (optional)</label>
-                                            <div class="bx--text-input__field-outer-wrapper">
+                                            <label htmlFor="phone" className="bx--label">Job role (optional)</label>
+                                            <div className="bx--text-input__field-outer-wrapper">
                                                 <div className="bx--text-input__field-wrapper">
-                                                    <input type="text" placeholder="Technology advisor" class="bx--text-input bx--text__input" title="" name="phone"/>
+                                                    <input type="text" placeholder="Technology advisor" className="bx--text-input bx--text__input" title="" name="phone"/>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
 
-                                    <div class="col-md-10 pb-4">
-                                        <div class="bx--form-item">
-                                            <label for="message" class="bx--label">Message</label>
-                                            <div class="bx--text-area__wrapper">
-                                                <textarea cols="50" rows="4" name="message" id="message" placeholder="Type your message here…" class="bx--text-area"></textarea>
+                                    <div className="col-md-10 pb-4">
+                                        <div className="bx--form-item">
+                                            <label htmlFor="message" className="bx--label">Message</label>
+                                            <div className="bx--text-area__wrapper">
+                                                <textarea cols="50" rows="4" name="message" id="message" placeholder="Type your message here…" className="bx--text-area"></textarea>
                                             </div>
                                         </div>
                                     </div>
 
-                                    <div class="col-md-5 pb-4"></div>
-                                    <div class="col-md-5 pb-4"><button type="submit" class="btn btn-primary share-btn col-md-6 float-right w-100 text-start">Submit</button></div>
+                                    <div className="col-md-5 pb-4"></div>
+                                    <div className="col-md-5 pb-4"><button type="submit" className="btn btn-primary share-btn col-md-6 float-right w-100 text-start">Submit</button></div>
 
 
                                 </div>
--- src/components/write.jsx
+++ src/components/write.jsx
@@ -10,9 +10,9 @@ export class Write extends React.Component {
 
                         <div className='col-md-2'>
                             <nav aria-label="breadcrumb">
-                                <ol class="breadcrumb">
-                                    <li class="breadcrumb-item"><a href="home">Home</a></li>
-                                    <li class="breadcrumb-item active" aria-current="page">Write</li>
+                                <ol className="breadcrumb">
+                                    <li className="breadcrumb-item"><a href="home">Home</a></li>
+                                    <li className="breadcrumb-item active" aria-current="page">Write</li>
                                 </ol>
                             </nav>
                         </div>
@@ -20,17 +20,17 @@ export class Write extends React.Component {
                         <div className='col-md-10 d-flex'>
                             <div className='col-md-5 leadspace leadspace-2 leadspace-padding'>
                                 <div className='col-md-8'>
-                                    <h1 class="leadSpace_header">Start writing, no matter what...
-                                    {/* Your writing inspires */}
+                                    <h1 className="leadSpace_header">Start writing, no matter what...
+                                        {/* Your writing inspires */}
                                     </h1>
                                     <p>We can do no great things, only small things with great love.</p>
                                 </div>
                             </div>
 
-                            <div class="col-md-6">
+                            <div className="col-md-6 d-none d-sm-block">
                                 <div role="presentation" className="col-md-7 float-end line_space Lines_container__36j4i">
-                                    <svg className="Lines_image__2O61U Lines_imageTop__27foq" height="0" viewBox="0 0 100 100" width="0" xmlns="http://www.w3.org/2000/svg"><line class="Lines_line__2-BFp" x1="-25%" y1="50%" x2="125%" y2="50%" vector-effect="non-scaling-stroke"></line></svg>
-                                    <svg className="Lines_image__2O61U Lines_imageBottom__12Ll5" height="0" viewBox="0 0 100 100" width="0" xmlns="http://www.w3.org/2000/svg"><line class="Lines_line__2-BFp" x1="-25%" y1="50%" x2="125%" y2="50%" vector-effect="non-scaling-stroke"></line></svg>
+                                    <svg className="Lines_image__2O61U Lines_imageTop__27foq" height="0" viewBox="0 0 100 100" width="0" xmlns="http://www.w3.org/2000/svg"><line className="Lines_line__2-BFp" x1="-25%" y1="50%" x2="125%" y2="50%" vectorEffect="non-scaling-stroke"></line></svg>
+                                    <svg className="Lines_image__2O61U Lines_imageBottom__12Ll5" height="0" viewBox="0 0 100 100" width="0" xmlns="http://www.w3.org/2000/svg"><line className="Lines_line__2-BFp" x1="-25%" y1="50%" x2="125%" y2="50%" vectorEffect="non-scaling-stroke"></line></svg>
                                 </div>
                             </div>
 
@@ -44,39 +44,39 @@ export class Write extends React.Component {
                     <form className='row m-0 p-0'>
                         <div className='col-md-2'></div>
                         <div className='col-md-9 pb-5'>
-                            {/* <div class="form_note float-start">All fields are required unless marked optional.</div> */}
+                            {/* <div className="form_note float-start">All fields are required unless marked optional.</div> */}
 
                             <div className='pt-3'>
-                                <div class="row row-cols-2">
+                                <div className="row">
 
-                                    <div class="col-md-10 pb-4">
+                                    <div className="col-md-10 pb-4">
                                         <div className="bx--form-item bx--text-input-wrapper">
-                                            <label for="firstName" className="bx--label">Title</label>
-                                            <div class="bx--text-input__field-outer-wrapper">
+                                            <label htmlFor="firstName" className="bx--label">Title</label>
+                                            <div className="bx--text-input__field-outer-wrapper">
                                                 <div className="bx--text-input__field-wrapper">
-                                                    <input type="text" placeholder="My funny story" class="bx--text-input bx--text__input" title="" name="firstName" />
+                                                    <input type="text" placeholder="My funny story" className="bx--text-input bx--text__input" title="" name="firstName" />
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
 
-                                    <div class="col-md-5 pb-4">
+                                    <div className="col-md-5 pb-4">
                                         <div className="bx--form-item bx--text-input-wrapper">
-                                            <label for="UserName" className="bx--label">User name</label>
-                                            <div class="bx--text-input__field-outer-wrapper">
+                                            <label htmlFor="UserName" className="bx--label">User name</label>
+                                            <div className="bx--text-input__field-outer-wrapper">
                                                 <div className="bx--text-input__field-wrapper">
-                                                    <input type="text" placeholder="John" class="bx--text-input bx--text__input" title="" name="userName" />
+                                                    <input type="text" placeholder="John" className="bx--text-input bx--text__input" title="" name="userName" />
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
 
-                                    <div class="col-md-5 pb-4">
+                                    <div className="col-md-5 pb-4">
                                         <div className="bx--form-item bx--text-input-wrapper">
-                                            <label for="tagName" className="bx--label">Tag name</label>
-                                            <div class="bx--text-input__field-outer-wrapper">
+                                            <label htmlFor="tagName" className="bx--label">Tag name</label>
+                                            <div className="bx--text-input__field-outer-wrapper">
                                                 <div className="bx--text-input__field-wrapper">
-                                                    <input type="text" placeholder="#fun #life" class="bx--text-input bx--text__input" title="" name="tagName" />
+                                                    <input type="text" placeholder="#fun #life" className="bx--text-input bx--text__input" title="" name="tagName" />
                                                 </div>
                                             </div>
                                         </div>
@@ -84,28 +84,28 @@ export class Write extends React.Component {
 
                                     <div className="col-md-10 pb-4">
                                         <div className="bx--form-item">
-                                            <label for="story" className="bx--label">Description</label>
+                                            <label htmlFor="story" className="bx--label">Description</label>
                                             <div className="bx--text-area__wrapper">
                                                 <textarea className="bx--text-area" cols="50" rows="15" name="story" placeholder="Type your description here…"></textarea>
                                             </div>
                                         </div>
                                     </div>
 
-                                    <div class="col-md-5 pb-4">
-                                        <div class="bx--form-item bx--form-item-checkbox bx--checkbox-wrapper">
-                                            <input type="checkbox" class="bx--checkbox" name="acknowledgement" />
-                                            <label for="anonymous" class="bx--checkbox-label text-start"><span class="bx--checkbox-label-text">Share as <i>anonymous</i>. </span></label>
+                                    <div className="col-md-5 pb-4">
+                                        <div className="bx--form-item bx--form-item-checkbox bx--checkbox-wrapper">
+                                            <input type="checkbox" className="bx--checkbox" name="acknowledgement" />
+                                            <label htmlFor="anonymous" className="bx--checkbox-label text-start"><span className="bx--checkbox-label-text">Share as <i>anonymous</i>. </span></label>
                                         </div>
                                     </div>
-                                    <div class="col-md-5 pb-4">
-                                        <button type="submit" class="btn btn-primary share-btn col-md-6 float-right w-100 text-start">Share</button>
+                                    <div className="col-md-5 pb-4">
+                                        <button type="submit" className="btn btn-primary share-btn col-md-6 float-right w-100 text-start">Share</button>
                                     </div>
 
 
                                 </div>
-                            </div>                  
-                            
-                            
+                            </div>
+
+
                         </div>
                     </form>
                 </div>
